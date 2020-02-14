c8f7f1a636a12beb0bc30598db8b671b13d120f3
j-j-dean <59969527+j-j-dean@users.noreply.github.com>
2020-02-14 13:42:19 -0500

    Update site.js
    New updates

--- site.js
+++ site.js
@@ -1,5 +1,23 @@
 
+
+var indexImage = {
+    /*************************************************************
+    * Update width of background image - removes horizontal scroll
+    **************************************************************/
+    UpdateImageWidth: function() {
+        if (document.title == "Jeff Dean Profile") {
+            $('#index-background-image').css("width",$('#index-main').css("width"));
+        }
+    }
+}
+
 $(document).ready(function() {
+
+    /**************************************************************************
+    * setup event listener to cut portion of image if resized
+    ***************************************************************************/
+    indexImage.UpdateImageWidth();
+    window.addEventListener('resize', indexImage.UpdateImageWidth);
 
     /**************************************************************************
      * the following function toggles the FDC testimonial popup under Careers 
